a5c699ddd48c1c999e8d8a9b1a9158534b245ce8
Lukman <lukmanbinharun@gmail.com>
2026-07-15 22:25:42 +0600

    return like details page

--- src/pages/SeoServicePage.tsx
+++ src/pages/SeoServicePage.tsx
@@ -1,5 +1,7 @@
 import PageMeta from "@/seo/PageMeta";
+import Section6 from "@/shared/sections/index-3/Section12";
 import Section1 from "@/shared/sections/seo-services-details/Section1";
+import Section2 from "@/shared/sections/seo-services-details/Section2";
 import Section3 from "@/shared/sections/seo-services-details/Section3";
 import Section4 from "@/shared/sections/seo-services-details/Section4";
 import Section5 from "@/shared/sections/seo-services-details/Section5";
@@ -9,9 +11,11 @@ export default function SeoServicePage() {
     <>
       <PageMeta title="Orisa - SEO" />
       <Section1 />
+      <Section2 />
       <Section3 />
       <Section4 />
       <Section5 />
+      <Section6 />
     </>
   );
 }
--- src/pages/Web-design-devlopment.tsx
+++ src/pages/Web-design-devlopment.tsx
@@ -1,10 +1,10 @@
 import PageMeta from "@/seo/PageMeta";
-import Section1 from "@/shared/sections/services-details/Section1";
-import Section2 from "@/shared/sections/services-details/Section2";
-import Section3 from "@/shared/sections/services-details/Section3";
-import Section4 from "@/shared/sections/services-details/Section4";
-import Section5 from "@/shared/sections/services-details/Section5";
 import Section6 from "@/shared/sections/index-3/Section12";
+import Section1 from "@/shared/sections/web-services-details/Section1";
+import Section2 from "@/shared/sections/web-services-details/Section2";
+import Section3 from "@/shared/sections/web-services-details/Section3";
+import Section4 from "@/shared/sections/web-services-details/Section4";
+import Section5 from "@/shared/sections/web-services-details/Section5";
 
 export default function ServicesDetailsPage() {
   return (
